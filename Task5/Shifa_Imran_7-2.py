7b805f41f1142743ccea81f49afd158f5c9d3da9
shifa309 <shifaimran309@gmail.com>
2023-03-21 19:57:17 +0500

    Task5

---
+++ Task5/Shifa_Imran_7-2.py
@@ -0,0 +1,7 @@
+group_size = int(input("How many people are in your dinner group? "))
+
+if group_size > 8:
+    print("Sorry, you'll have to wait for a table.")
+else:
+    print("Your table is ready. Enjoy your meal!")
+
---
+++ Task5/Shifa_Imran_7-3.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number: "))
+
+if number % 10 == 0:
+    print(number, "is a multiple of 10.")
+else:
+    print(number, "is not a multiple of 10.")
